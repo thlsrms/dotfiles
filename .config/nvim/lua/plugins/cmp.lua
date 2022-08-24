ca295a4364d4f4a6e11628b23c67886825f79192
Thales Ramos <thlsrmsdev@gmail.com>
2022-08-24 18:20:19 +0200

    nvim: Invert doc scroll keymap

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -27,8 +27,8 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<up>'] = cmp.mapping.select_prev_item(),
     ['<down>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
