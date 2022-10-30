4998af83ad8e0b19711a612e5bf72ec73cf7ff88
Thales Ramos <thlsrmsdev@gmail.com>
2022-10-30 10:57:38 +0100

    nvim: Updating mason plugin LSP server names

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,62 +1,61 @@
 local mason_status_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_status_ok then
-  vim.notify("Couldn't load Mason-LSP-Config" .. mason_lspconfig, "error")
+    vim.notify("Couldn't load Mason-LSP-Config" .. mason_lspconfig, "error")
 end
 
 require("mason").setup {
-  ui = {
-    icons = {
-      package_installed = "✓"
+    ui = {
+        icons = {
+            package_installed = "✓"
+        }
     }
-  }
 }
 
 -- Extension to bridge mason.nvim with the lspconfig plugin
 mason_lspconfig.setup({
-  -- A list of servers to automatically install if they're not alredy installed.
-  ensure_installed = {
-    "bash-language-server",
-    "typescript-language-server",
-    "tailwindcss-language-server",
-    "emmet-ls",
-    "eslint-lsp",
-    "rust-analyzer",
-    "selene",
-    "lua-language-server",
-    "python-lsp-server",
-    "pyright",
-    "sqlls",
-    "svelte-language-server",
-    "omnisharp",
-    "cpptools",
-    "codelldb",
-    "deno",
-    "dockerfile-language-server",
-    "html-lsp",
-    "css-lsp",
-  }
+    -- A list of servers to automatically install if they're not alredy installed.
+    ensure_installed = {
+        "bashls",
+        "tsserver",
+        "tailwindcss",
+        "emmet_ls",
+        "eslint",
+        "rust_analyzer",
+        "sumneko_lua",
+        "pyright",
+        "sqlls",
+        "svelte",
+        "omnisharp",
+        "clangd",
+        "dockerls",
+        "html",
+        "cssls",
+    }
 })
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
-  vim.notify("Couldn't load LSP-Config" .. lspconfig, "error")
+    vim.notify("Couldn't load LSP-Config" .. lspconfig, "error")
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 mason_lspconfig.setup_handlers({
-  -- The first entry (without a key) will be the default handler
-  -- and will be called for each installed server that doesn't have
-  -- a dedicated handler.
-  function(server_name) -- Default handler
-    lspconfig[server_name].setup {
-      capabilities = capabilities
-    }
-  end,
+    -- The first entry (without a key) will be the default handler
+    -- and will be called for each installed server that doesn't have
+    -- a dedicated handler.
+    function(server_name) -- Default handler
+        lspconfig[server_name].setup {
+            capabilities = capabilities
+        }
+    end,
 
-  -- Next, provide targeted overrides for specific servers.
-  -- For example, a handler override for the `rust_analyzer`:
-  ["rust_analyzer"] = function()
-    require('rust-tools').setup({})
-  end,
+    -- Next, provide targeted overrides for specific servers.
+    -- For example, a handler override for the `rust_analyzer`:
+    ["rust_analyzer"] = function(server_name)
+        lspconfig[server_name].setup {
+            capabilities = capabilities
+        }
+        require('rust-tools').setup({})
+    end,
 })
