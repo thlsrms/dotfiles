f22b442982b30baf17fad0999715358d7c3b9e50
Thales Ramos <thlsrmsdev@gmail.com>
2022-10-31 22:10:51 +0100

    SurfingKeys: Revert most settings to default

--- .config/surfingkeys_config.js
+++ .config/surfingkeys_config.js
@@ -3,7 +3,6 @@
 // Web Browser extension to navigate the web using VIM-like keybindings
 // ----
 
-/*-- Temporary config file until I organize a better way to write this configuration --*/
 const {
     aceVimMap,
     mapkey,
@@ -28,43 +27,10 @@ const {
     RUNTIME
 } = api;
 
-/*---------- DVORAK layout configuration ---------- */
 //-- Navigation remaps
 /* Show hints */
-map('<Alt-h>', 'f'); // Show link hints. -- Hold <space> to hide hints
+map('u', 'f'); // Show link hints. -- Hold <space> to hide hints
 
-map('u', 't'); // Open URL
-
-/* Tabs */
-map('gt', 'T'); // Go to tab
-map('<Alt-t>', 'E'); // Go one tab Left
-map('<Alt-n>', 'R'); // Go one tab Right
-
-map('<Alt-c>', 'S'); // Go back in history
-
-/* Search key */
-map('p', 's');
-map('K', 'N'); // Previous found text
-map('J', 'n'); // Next found text
-
-vmap('sg', 'pg'); // Search selected with google
-
-/* Scroll */
-map('T', 'd'); // Scroll half down
-map('N', 'e'); // Scroll half up
-map('t', 'j'); // Scroll down
-vmap('t', 'j');
-map('n', 'k'); // Scroll up
-vmap('n', 'k');
-map('s', 'l'); // Scroll right
-vmap('s', 'l');
-map('H', '0'); // Scroll all the way to the left
-vmap('H', '0');
-map('S', '$'); // Scroll all the way to the right
-vmap('S', '$');
-
-map('R', 'r'); // Reload page
-
-mapkey('U','#8Open an URL in current tab', function() {
+mapkey('gO','#8Open an URL in current tab', function() {
   Front.openOmnibar({type: "URLs", extra: "getTopSites", tabbed: false});
 });
