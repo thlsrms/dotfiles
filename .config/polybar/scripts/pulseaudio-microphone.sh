77c98daa88911be8addb2aae59b1a5836a077f57
Thales Ramos <thlsrmsdev@gmail.com>
2022-12-18 20:44:51 +0100

    polybar: Mic control changed from pulseaudio to pipewire

--- .config/polybar/scripts/pulseaudio-microphone.sh
+++ .config/polybar/scripts/pulseaudio-microphone.sh
@@ -1,62 +1,54 @@
 #!/bin/sh
 
-status() {
-  MUTED=$(pacmd list-sources | awk '/\*/,EOF {print}' | awk '/muted/ {print $2; exit}')
+status () {
+    MUTED=$(wpctl get-volume @DEFAULT_SOURCE@ | grep 'MUTED')
 
-  if [ "$MUTED" = "yes" ]; then
-    echo "%{u#ab2115}%{+u}%{F#f00}  mtd "
-  else
-    VOLUME=$(pacmd list-sources | grep "\* index:" -A 7 | grep volume | awk -F/ '{print $2}' | tr -d ' ' ) 
-    echo "%{u#3384d0}%{+u}   $VOLUME "
-  fi
+    if test -z "$MUTED"
+    then
+        VOLUME=$(wpctl get-volume @DEFAULT_SOURCE@ | tr -d 'Volume: ' | awk '{print $1 * 100}') 
+        if  [[ $VOLUME -gt 60 ]]; then
+            # Limit mic volume to 0.60 to fight the mic boost, until a better solution is found
+            wpctl set-volume @DEFAULT_SOURCE@ 0.60
+        fi
+
+        echo "%{u#3384d0}%{+u}   $VOLUME "
+    else
+        echo "%{u#ab2115}%{+u}%{F#f00}  mtd "
+    fi
 }
 
 listen() {
     status
-
     LANG=EN; pactl subscribe | while read -r event; do
         if echo "$event" | grep -q "source" || echo "$event" | grep -q "server"; then
-            # Making sure the correct input device is the default
-            #pacmd set-default-source 1
-            # Keeping the volume low
-            pacmd set-source-volume alsa_input.pci-0000_00_1b.0.analog-stereo 19660
             status
         fi
     done
 }
 
 toggle() {
-  MUTED=$(pacmd list-sources | awk '/\*/,EOF {print}' | awk '/muted/ {print $2; exit}')
-  DEFAULT_SOURCE=$(pacmd list-sources | awk '/\*/,EOF {print $3; exit}')
-
-  if [ "$MUTED" = "yes" ]; then
-      pactl set-source-mute "$DEFAULT_SOURCE" 0
-  else
-      pactl set-source-mute "$DEFAULT_SOURCE" 1
-  fi
+    wpctl set-mute @DEFAULT_SOURCE@ toggle
 }
 
 increase() {
-  DEFAULT_SOURCE=$(pacmd list-sources | awk '/\*/,EOF {print $3; exit}')
-  pactl set-source-volume "$DEFAULT_SOURCE" +5%
+    wpctl set-volume @DEFAULT_SOURCE@ 0.02+
 }
 
 decrease() {
-  DEFAULT_SOURCE=$(pacmd list-sources | awk '/\*/,EOF {print $3; exit}')
-  pactl set-source-volume "$DEFAULT_SOURCE" -5%
+    wpctl set-volume @DEFAULT_SOURCE@ 0.02-
 }
 
 case "$1" in
     --toggle)
         toggle
-        ;;
+    ;;
     --increase)
         increase
-        ;;
+    ;;
     --decrease)
         decrease
-        ;;
+    ;;
     *)
         listen
-        ;;
+    ;;
 esac
