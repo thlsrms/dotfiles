db5d907ad1c61573a4be266de9442d489a7465a9
Thales Ramos <thlsrmsdev@gmail.com>
2023-03-19 14:51:46 +0100

    nvim: cmp formatting options

--- .config/nvim/after/plugin/lsp-cmp.lua
+++ .config/nvim/after/plugin/lsp-cmp.lua
@@ -22,12 +22,11 @@ local cmp_opts = {
             vim.fn["vsnip#anonymous"](args.body)
         end
     },
-
     -- Mapping settings
     mapping = cmp.mapping.preset.insert({
         ['<up>'] = cmp.mapping.select_prev_item(),
         ['<down>'] = cmp.mapping.select_next_item(),
-        ['<C-f>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs( -4),
         ['<C-d>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
@@ -35,7 +34,6 @@ local cmp_opts = {
         ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
         ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
     }),
-
     formatting = {
         format = function(entry, vim_item)
             vim_item.kind = lspkind.symbolic(vim_item.kind, { with_text = true })
@@ -54,18 +52,16 @@ local cmp_opts = {
             return vim_item
         end
     },
-
     -- Specify installed sources
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'vsnip' },
         { name = 'cmp_tabnine', max_item_count = 8 },
-        { name = 'buffer', keyword_length = 5 },
+        { name = 'buffer',      keyword_length = 5 },
         { name = 'path' },
         { name = 'calc' },
         { name = 'nvim_lua' },
     }),
-
     experimental = {
         native_menu = false,
         ghost_text = true,
@@ -73,10 +69,10 @@ local cmp_opts = {
 }
 
 tabnine:setup({
-    max_lines = 500;
-    max_num_results = 5;
-    sort = true;
-    show_prediction_strength = true;
+    max_lines = 500,
+    max_num_results = 5,
+    sort = true,
+    show_prediction_strength = true,
 })
 
 cmp.setup(cmp_opts)
