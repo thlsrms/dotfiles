d59a596d263eef0baebc577ce6d16a5dc2933218
Thales Ramos <thlsrmsdev@gmail.com>
2023-03-19 14:30:18 +0100

    fish: Add `ov` function - Find and open dir on neovim

--- .config/fish/functions/my_aliases.fish
+++ .config/fish/functions/my_aliases.fish
@@ -1,8 +1,26 @@
 #!/bin/sh fish
 # My aliases
 
-function vim -d 'alias vim=nvim'
-	command nvim $argv;
+function ov -d 'Open neovim and \":cd (directory)\" or open a new buffer if fzf is cancelled'
+    if count $argv > /dev/null
+        command nvim $argv;
+    else
+        # Find a directory to open with neovim using fd
+        set -l dir (fd . ~/. /zdrive -H -t directory \
+            # exclude some of my dirs
+            -E "*cache" \
+            -E "*.git" \
+            -E "*.stack" \
+            -E "*node_modules" \
+            -E "*target" \
+            -E "*.cargo" \
+            -E "*go/pkg/mod" \
+            -E "*github*" \
+            -E "*Games*" \
+            -E "*Downloads*" \
+        | fzf)
+        command nvim $dir/. -c ":cd $dir";
+    end
 end
 
 function rg -d 'alias rg=rg --sort path'
