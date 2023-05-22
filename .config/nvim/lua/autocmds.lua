c28dda12e4729ea3ba6482d5aa490fb0c1ed2a77
Thales Ramos <thlsrmsdev@gmail.com>
2023-05-22 10:54:18 +0200

    nvim: Add highlight on yank

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,2 +1,3 @@
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+vim.cmd [[au BufWritePre * lua vim.lsp.buf.format()]]
 vim.cmd [[au BufNewFile,BufRead *.wgsl set filetype=wgsl]]
+vim.cmd [[au TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=100 }]]
