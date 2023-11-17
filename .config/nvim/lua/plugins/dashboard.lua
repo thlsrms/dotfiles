c179144b34c46a0f5a0ffe4148d39ebf3ab3f488
Thales R <thlsrmsdev@gmail.com>
2023-11-17 19:09:43 +0100

    nvim: Dashboard - center shortcut actions

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -2,7 +2,70 @@ return {
   "nvimdev/dashboard-nvim",
   keys = { { "<leader>uD", "<cmd>Dashboard<cr>", desc = "Dashboard" } },
   opts = function(_, opts)
-    local logo = [[lazyVim]]
-    opts.config.header = vim.split(logo, "\n", { trimempty = true })
+    -- empty space on logo to push the center down
+    local logo = [[
+
+
+
+
+
+
+
+
+
+
+    ]]
+    local center = {
+      {
+        action = "telescope find_files",
+        desc = " find file",
+        icon = " ",
+        key = "f",
+      },
+      {
+        action = "ene | startinsert",
+        desc = " new file",
+        icon = " ",
+        key = "n",
+      },
+      {
+        action = "telescope oldfiles",
+        desc = " recent files",
+        icon = " ",
+        key = "r",
+      },
+      {
+        action = "telescope live_grep",
+        desc = " find text",
+        icon = " ",
+        key = "g",
+      },
+      {
+        action = "Telescope projects",
+        desc = " projects",
+        icon = " ",
+        key = "p",
+      },
+      {
+        action = [[lua require("lazyvim.util").telescope.config_files()()]],
+        desc = " config",
+        icon = " ",
+        key = "c",
+      },
+      {
+        action = "SessionLoad ",
+        desc = " restore session",
+        icon = " ",
+        key = "s",
+      },
+      {
+        action = "qa",
+        desc = " quit",
+        icon = " ",
+        key = "q",
+      },
+    }
+    opts.config.header = vim.split(logo, "\n")
+    opts.config.center = center
   end,
 }
