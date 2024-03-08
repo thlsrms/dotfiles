b61ed60e25f91f346d13ec6dbcb2ae7397d23fb6
Thales R <thlsrmsdev@gmail.com>
2024-03-08 20:48:46 +0100

    nvim: Add keymap to close buffer through telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,14 @@ return {
         layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending",
         winblend = 50,
+        mappings = {
+          n = {
+            ["<S-d>"] = "delete_buffer",
+          },
+          i = {
+            ["<C-S-d>"] = "delete_buffer",
+          },
+        },
       },
     })
   end,
