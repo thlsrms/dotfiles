e44acf1d383ebcfba18570deb4e5dbd25eead1df
Thales R <thlsrmsdev@gmail.com>
2024-03-08 15:11:28 +0100

    nvim: Add keymap to toggle stay-centered

--- .config/nvim/lua/plugins/stay-centered.lua
+++ .config/nvim/lua/plugins/stay-centered.lua
@@ -1,5 +1,14 @@
 return {
   "arnamak/stay-centered.nvim",
+  keys = {
+    {
+      "<leader>um",
+      function()
+        require("stay-centered").toggle()
+      end,
+      desc = "Toggle Stay Centered",
+    },
+  },
   config = function()
     require("stay-centered").setup({})
   end,
