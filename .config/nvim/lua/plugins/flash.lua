7d1b0befa9af9f42ce5cd6047d8287e03a49684b
Thales R <thlsrmsdev@gmail.com>
2024-03-08 15:09:02 +0100

    nvim: Remap flash key to <A-s> to avoid clashes

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -4,4 +4,47 @@ return {
   opts = {
     labels = "aoeuhtns,.crjkbm",
   },
+  keys = {
+    { "s", mode = { "n", "x", "o" }, false },
+    {
+      "<A-s>",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "<A-S>",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+    {
+      "r",
+      mode = "o",
+      function()
+        require("flash").remote()
+      end,
+      desc = "Remote Flash",
+    },
+    {
+      "R",
+      mode = { "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Treesitter Search",
+    },
+    {
+      "<A-/>",
+      mode = { "c" },
+      function()
+        require("flash").toggle()
+      end,
+      desc = "Toggle Flash Search",
+    },
+  },
 }
