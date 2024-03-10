d473eceb07430b9462275cfe4a128b7446e00ab1
Thales R <thlsrmsdev@gmail.com>
2024-03-10 14:34:53 +0100

    nvim: Disable colorscheme plugins

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,6 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
 }
