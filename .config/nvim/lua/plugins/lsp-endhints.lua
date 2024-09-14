243fb08151617a13e5cfb53223ee00ac89f3080f
Thales R <thlsrmsdev@gmail.com>
2024-09-14 13:06:35 +0200

    nvim: Add 'lsp-endhints' - inlay-hints at the EOL

---
+++ .config/nvim/lua/plugins/lsp-endhints.lua
@@ -0,0 +1,22 @@
+return {
+  "chrisgrieser/nvim-lsp-endhints",
+  event = "LspAttach",
+  opts = {},
+  config = function()
+    -- default settings
+    require("lsp-endhints").setup({
+      icons = {
+        type = "󰗴 ",
+        parameter = "󰉽 ",
+        offspec = " ", -- hint kind not defined in official LSP spec
+        unknown = " ", -- hint kind is nil
+      },
+      label = {
+        padding = 1,
+        marginLeft = 0,
+        bracketedParameters = true,
+      },
+      autoEnableHints = true,
+    })
+  end,
+}
